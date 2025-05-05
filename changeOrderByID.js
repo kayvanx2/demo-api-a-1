212a1f9be081f9b5bdfbacd5b25339dcaf942574
kayvanx2 <kevin.farzaneh+git2@gmail.com>
2025-05-06 09:40:26 +1000

    Update changeOrderByID.js

--- changeOrderByID.js
+++ changeOrderByID.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/api/changeOrderById', (req, res) => {
+app.post('/api/v2/changeOrderById', (req, res) => {
   const {
     'customer-id': customerId,
     email,
